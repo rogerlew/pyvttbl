cafd70cad3425da3e53e31165ac41c0a96eb6622
roger <roger@bearnote>
2011-05-23 17:43:56 -0700

    .

---
+++ __init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2011, Roger Lew [see LICENSE.txt]
+# This software is funded in part by NIH Grant P20 RR016454.
+
+from pyvttbl import DataFrame
+from pyvttbl import PyvtTbl
+from pyvttbl import Marginals
+from pyvttbl import Histogram
+from pyvttbl import Descriptives
+from anova import Anova
+
+__all__ = ["DataFrame",
+           "PyvtTbl",
+           "Marginals",
+           "Descriptives",
+           "Histogram",
+           "Anova"]
